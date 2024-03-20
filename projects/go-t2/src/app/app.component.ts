ad0e165464b5f01f2f89056006c0166b8f6bf3e7
FerreiroAlberto <homelandz@gmail.com>
2024-03-20 21:49:09 +0100

    Add kill feature

--- projects/go-t2/src/app/app.component.ts
+++ projects/go-t2/src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { ListComponent } from "./list/list.component";
 
 @Component({
-  selector: 'isdi-root',
+  selector: "isdi-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, ListComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'GoT2';
+  title = "GoT2";
 }
